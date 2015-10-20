2f9fa44078c66f534c82d90c42ea31651076d1d0
chn.challenger <chn.challenger@gmail.com>
2015-10-20 16:06:40 +0100

    passing first test on protractor

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -1,7 +1,11 @@
-describe('Github Profile finder', function() {
-  it('has a title', function() {
+describe('GitHub profile finder', function() {
+  it('finds profiles', function() {
     browser.get('http://localhost:8080');
 
-    expect(browser.getTitle()).toEqual('Github user search');
+    element(by.model('searchCtrl.searchTerm')).sendKeys('spike01');
+    element(by.className('btn')).click();
+
+    expect(element(by.binding('user.login')).getText()).
+        toEqual('spike01');
   });
 });
