8dd4fb1db7eef29d0e1ee0b9ae63d01ce0a8267b
Tim Hyson <tjhyson@gmail.com>
2015-10-20 15:06:25 +0100

    api token added and working

--- js/searchFactory.js
+++ js/searchFactory.js
@@ -1,5 +1,5 @@
 githubUserSearch.factory('Search', ['$http', function($http) {
-  var queryUrl = 'https://api.github.com/search/users';
+  var queryUrl = 'https://api.github.com/search/users?access_token=' + access_token;
   return {
     query: function(searchTerm) {
       return $http({
